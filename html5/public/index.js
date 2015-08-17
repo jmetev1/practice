d079b9eb90c7a5e19d2d0bc8497290dd590bb5e5
Chad Lee <chad.lee@archoninfosys.com>
2015-08-17 17:15:09 -0500

    Use accounting.js for formatting AND parsing

--- html5/public/index.js
+++ html5/public/index.js
@@ -1,5 +1,5 @@
 var $ = require("jquery");
-var accounting = require("accounting"); //HINT: use this for currency formatting (http://openexchangerates.github.io/accounting.js/)
+var accounting = require("accounting"); //HINT: use this for currency formatting/parsing (http://openexchangerates.github.io/accounting.js/)
 
 $(function() {
 	//the document is ready; write some code
