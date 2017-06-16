1abc94805e2e2a7dff640f80af315ea75799ac21
Chad Lee <chad.lee@archoninfosys.com>
2017-06-16 09:46:53 -0500

    Adding eslint with civicsource config

--- html5/webpack.config.babel.js
+++ html5/webpack.config.babel.js
@@ -1,7 +1,6 @@
-const path = require("path");
-const webpack = require("webpack");
+import path from "path";
 
-module.exports = {
+export default {
 	entry: "./public/index.js",
 	devServer: {
 		port: 1367,
